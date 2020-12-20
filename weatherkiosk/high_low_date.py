11c1c8b1730ec98dc14bf515aa8e2cdba29b22b7
outlaws42 <outlaws42@gmail.com>
2020-12-20 06:03:09 -0500

    Clearified methods in high_low_date.py

--- weatherkiosk/high_low_date.py
+++ weatherkiosk/high_low_date.py
@@ -10,7 +10,7 @@ import pytz
 from settings import DB_URI, DATABASE
 
 # The date needed for misssing high low temp in string form and 'YYYY-MM-DD'
-missing_date = '2020-12-18' 
+missing_date = '2020-12-15' 
 
 # Database info
 mongo = MongoClient(DB_URI)
@@ -38,19 +38,31 @@ class GetHighLow():
       )
 
   def get_high_low_temp_db(
-    self, coll_read, coll_write, find_key, sort_key, date_):
-    """ Gets High and Low temp for the day. only between 11:30pm and 12pm """
-    high_low = self.get_high_low_today(coll_read, find_key, sort_key, date_)
-    self.write_one_db(coll_write, high_low)
+    self, 
+    coll_read, 
+    coll_write, 
+    find_key, 
+    sort_key, 
+    str_date
+    ):
+    """ Gets High and Low temp for the day. 
+        only between 11:30pm and 12pm """
+    high_low = self.get_high_low_today(
+      coll_read, 
+      find_key, 
+      sort_key, 
+      str_date
+      )
+    # self.write_one_db(coll_write,high_low)
     print(high_low) 
     print(f'write document to the {coll_write} collection via get_high_low_temp_db')
 
 
-  def high_low_list(self, listin, key):
+  def high_low_list(self, listin, sort_key):
     """ sort list by a key in the dictionaries
         Then get first and last dictionary from the list """
     listout = []
-    listin.sort(key=lambda item: item.get(key))
+    listin.sort(key=lambda item: item.get(sort_key))
     listout.append(listin[-1])
     listout.append(listin[0])
     return listout
@@ -62,21 +74,21 @@ class GetHighLow():
       date(int(year), int(month), int(day)), time())
     return wanted_date
 
-  def get_doc_today_db(self, col, key, date_):
+  def get_doc_today_db(self, col_read, find_key, str_date):
     """ Gets Documents from today mongoDB database """
-    today = self.date_request(date_)
+    today = self.date_request(str_date)
     tomorrow = today + timedelta(1)
-    collection = db[col]
+    collection = db[col_read]
     aware_times = collection.with_options(
           codec_options=CodecOptions(tz_aware=True,tzinfo=pytz.timezone('US/Eastern')))
-    response = aware_times.find({key :{'$lt' : tomorrow, '$gte' : today}})
+    response = aware_times.find({find_key :{'$lt' : tomorrow, '$gte' : today}})
     results = [doc for doc in response]
     return results
   
-  def get_high_low_today(self, col,find_key, sort_key,date_):
-    today_doc = self.get_doc_today_db(col, find_key,date_)
+  def get_high_low_today(self, col_read,find_key, sort_key,str_date):
+    today_doc = self.get_doc_today_db(col_read, find_key,str_date)
     temp_list = self.high_low_list(today_doc, sort_key)
-    today = self.date_request(date_)
+    today = self.date_request(str_date)
     high_low = {
               'icon' : temp_list[0]['current_icon'],
               'high' : temp_list[0]['current_temp'], 
