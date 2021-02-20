4a09d171f44f39beefe35dca9e34bf63a27a2910
outlaws42 <outlaws42@gmail.com>
2021-02-20 06:21:27 -0500

    Removed not needed shell script

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/mnt/sda7/troy/Documents/Projects/VirtualEnv/weatherKiosk/bin/python3.-u./mnt/sda7/troy/Documents/Projects/weather_kiosk/get_weather.py
-/mnt/sda7/troy/Documents/Projects/VirtualEnv/weatherKiosk/bin/python3.-u./mnt/sda7/troy/Documents/Projects/weather_kiosk/indoor.py
-
